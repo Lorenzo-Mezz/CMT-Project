520c48cf0e0a42c7bef0868fedf66b59481b6dfc
Lorenzo-Mezz <lorenzo.mezzanotteamat@epfl.ch>
2025-11-28 10:54:55 +0100

    Add files via upload

--- tabletempandco2.m
+++ tabletempandco2.m
@@ -1,3 +1,3 @@
 co2_future_mod = co2_future(11:310, :)
-csvco2andtemp = table(future_dates', Y_future, co2_future_mod.CO2, 'VariableNames', {'Time','Surface Temp','Atm. CO2'})
+csvco2andtemp = table(future_dates', Y_future, co2_future_mod.CO2, bottom_pred, 'VariableNames', {'Time','Surface Temp','Atm. CO2', 'Bottom Temp'})
 writetable(csvco2andtemp, 'co2_temp_for_c.csv')
