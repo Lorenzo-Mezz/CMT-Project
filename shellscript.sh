2cf1c3e3d975728190e995ffa1130dda28e4afc2
Lorenzo-Mezz <lorenzo.mezzanotteamat@epfl.ch>
2025-12-12 17:40:12 +0100

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,3 +1,3 @@
 /usr/local/bin/matlab-2021b -batch "run('src/tabletempandco2.m')"
-gcc ./src/projet.c -o ./bin/projet -lm && ./projet
+gcc ./src/projet.c -o ./bin/projet -lm && ./bin/projet
 /usr/local/bin/matlab-2021b -batch "run('src/plot_graphs.m')"
