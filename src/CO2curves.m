c50ed34c96bfb85ae3a8715cc00b0821b144dab2
Lorenzo-Mezz <lorenzo.mezzanotteamat@epfl.ch>
2025-12-17 14:10:47 +0100

    final commit

--- src/CO2curves.m
+++ src/CO2curves.m
@@ -1,16 +1,20 @@
-co2_data = readtable('../data/co2_annmean_mlo.csv');
+% read CSV file and clean data
+co2_data = readtable('../data/co2_annmean_mlo.csv'); 
 co2_data.x______________________________________________________________ = [];
 co2_data(1:29,:) = [];
 c = split(co2_data.Var1, ',');
 c(:,3) = num2cell(zeros(size(c, 1), 1));
-
 year = str2double(c(:,1));
 co2_cc = str2double(c(:,2));
+
+% create clean table
 c_table = table(year, co2_cc, 'VariableNames', {'Annee', 'CO2'});
 
+% fit a 2nd-degree polynomial (quadratic trend) to historical CO2 data
 model = polyfit(year, co2_cc, 2);
 disp(model)
 
+% time vector and generating monthly intervals
 t = []
 future_years = (2025:2050)';
 t_min = min(year);  
@@ -20,13 +24,12 @@ for j = 1: length(future_years)
     end
 end
 
-co2_pred = polyval(model, t)
-
-mean_pred = polyval(model, t);
+% predict future CO2 concentrations using the polynomial model and store in table
+co2_pred = polyval(model, t);
 co2_future = table(t, co2_pred, 'VariableNames', {'Annee','CO2'});
-disp(co2_future)
-c = [c_table; co2_future];
 
+% combine historical data with future predictions and plot
+c = [c_table; co2_future];
 figure; 
 plot(c.Annee, c.CO2)
 
